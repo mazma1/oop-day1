d179e288115da1b35abb79ddfccdcfa9f58a88b3
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-06 15:33:00 +0100

    Create super class Question

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@
 // A QUIZ APP FOR STUDENTS
 
 class Student {
-  
   constructor (studentName, studentEmail) {
     this.name = studentName;
     this.email = studentEmail;
@@ -18,5 +17,33 @@ class Student {
     let scores = this.quizScores.length > 0 ? this.quizScores.join(",") : "No Scores Available";
     return this.name + "'s scores: " + scores;
   }
+}
+
+// Super Parent Class
+class Question {
+    constructor (theQuestion, theOptions, theCorrectAnswer) {
+        this.question = theQuestion;
+        this.options = theOptions;
+        this.correctAnswer = theCorrectAnswer;
+        this.studentAnswer = "";
+    }  
+  
+    getQuizDate () {
+        let newDate = new Date();
+        const QUIZ_CREATED_DATE = newDate.toDateString();
+        
+        return ("Quiz Created On: " + QUIZ_CREATED_DATE);
+    }
+  
+    displayQuestion () {
+        let choiceCounter = 1;
+        console.log (this.question);
+
+        this.options.forEach(function (eachOption)  {
+            console.log(choiceCounter + ")" + eachOption);
+            choiceCounter++;
+            
+        });
+    }
   
 }
